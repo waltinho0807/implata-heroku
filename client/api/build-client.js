f813b703af1ed3b630afd6a99c7940ad76adf359
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-08-31 01:50:16 -0300

    config doamain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.calegaritest.com.br',
       headers: req.headers
     });
   } else {
