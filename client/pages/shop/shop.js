5d2387e6c3e6049abc97db9ff192615a15003210
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-08-31 10:37:20 -0300

    front v2

--- client/pages/shop/shop.js
+++ client/pages/shop/shop.js
@@ -8,7 +8,7 @@ import Header from '../components/Header';
 import Bread from '../../components/shop/Bread';
 import Shop from '../../components/shop/Shop';
 
-const Shop = ()=>{
+const Loja = ()=>{
     return (
         <div>
             <Head/>
