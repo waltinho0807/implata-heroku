b55634ba771516f1ee5f3dcf64296be255923532
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-08-31 10:49:01 -0300

    shop v2

--- client/pages/shop/shop.js
+++ client/pages/shop/shop.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Head from '../components/Head';
-import Footer from '../components/Footer';
-import Javascript from '../components/Javascript';
-import Header from '../components/Header';
+import Head from '../../components/Head';
+import Footer from '../../components/Footer';
+import Javascript from '../../components/Javascript';
+import Header from '../../components/Header';
 
 import Bread from '../../components/shop/Bread';
 import Shop from '../../components/shop/Shop';
