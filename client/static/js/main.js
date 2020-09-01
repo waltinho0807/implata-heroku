3dcbf6c78b902702270536ae98c2a17a59f960f1
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-08-31 22:46:15 -0300

    commit

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -10,6 +10,11 @@
 'use strict';
 
 (function ($) {
+    document.addEventListener("touchstart", function(e) {
+        console.log(e.defaultPrevented);  // will be false
+        e.preventDefault();   // does nothing since the listener is passive
+        console.log(e.defaultPrevented);  // still false
+    }, Modernizr.passiveeventlisteners ? {passive: true} : false);
 
     /*------------------
         Preloader
