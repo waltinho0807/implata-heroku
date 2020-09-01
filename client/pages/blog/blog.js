32553c6f473e83877af8cadc4cf239da97a4a2b6
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-08-31 21:14:26 -0300

    hhhhhhh

--- client/pages/blog/blog.js
+++ client/pages/blog/blog.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import Head from '../../components/Head';
-import Footer from '../../components/Footer';
-import Javascript from '../../components/Javascript';
-import Header from '../../components/Header';
 
 import HeroPages from '../../components/form/HeroPages';
 
@@ -18,8 +14,7 @@ const BlogPage = ({currentUser})=>{
             <BreadBlog />
             <Blog/>
             <BlogRelated/>
-            <Footer/>
-            <Javascript/>
+            
         </div>
     )
    
